3e50670a82aff632bf7773c93fce0631bfb5be8f
franvazquezz <franvazquez2001@gmail.com>
2023-06-26 13:45:22 -0300

    deploy 3.3

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: true,
-    host: 'https://creative-canines.up.railway.app',
-  },
+  base:  'https://creative-canines.up.railway.app/'
 })
